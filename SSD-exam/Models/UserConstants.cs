5598c5ca2c0757b137fce144bba8af26c9d5097d
abdi2148 <abdiqafar.abas@gmail.com>
2022-06-02 19:35:08 +0100

    Upload (jwt complete)

---
+++ SSD-exam/Models/UserConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtApp.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "AdminAnn", EmailAddress = "ann.admin@email.com", Password = "1234", GivenName = "Ann", Surname = "Marie", Role = "Administrator" },
+            new UserModel() { Username = "UserJoe", EmailAddress = "joe.seller@email.com", Password = "1234", GivenName = "Joe", Surname = "Doe", Role = "Seller" },
+        };
+    }
+}
